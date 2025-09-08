4e5c5af4d75c7a1f840fa27ffb440a4d610054fb
crooksnwhiskey <alex.chardon36@gmail.com>
2025-09-08 12:28:17 -0400

    added comments to the blank page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,20 +1,29 @@
 /**
- * Function calls
+ * The Blank Page
  * Alex Chardon
  * 
- * working on function calls examples.
+ * A piece of paper on a brown desk
+ * non-interactive
  */
 
 "use strict";
 
-
+/**
+ * Creates the canvas
+ */
 function setup() {
     //once at the beginning of the program
+    //create the canvas at normal resolution
     createCanvas(640, 480);
 }
 
+/**
+ * Draws the desk ad paper
+ */
 function draw() {
     // Every frame
+    //the brown desk
     background(73, 64, 58);
+    //the blank sheet of paper
     rect(200, 80, 240, 320);
 }
