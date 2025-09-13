ef2a3556d6b061cb27619518210ebdbb296f3578
crooksnwhiskey <alex.chardon36@gmail.com>
2025-09-12 22:00:23 -0400

    used variables to change cursor

--- topics/instructions/introducing-variables/js/script.js
+++ topics/instructions/introducing-variables/js/script.js
@@ -10,13 +10,19 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(500, 500,);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+draws circle*/
 function draw() {
+    background(53, 74, 120,);
+
+    push();
+    fill(mouseX, mouseY, 1000);
+    noStroke();
+    ellipse(mouseX, mouseY, 100, 100);
+    pop();
 
 }
