461be97d2a080988ab19a151c8d975e017304364
crooksnwhiskey <alex.chardon36@gmail.com>
2025-09-12 21:41:09 -0400

    learning variables

---
+++ topics/instructions/introducing-variables/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * variables
+ * Alex Chardon
+ * learning variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
