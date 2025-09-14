0a6a79e5b423ea194fac220a99dd46e1b4de0725
crooksnwhiskey <alex.chardon36@gmail.com>
2025-09-14 10:45:33 -0400

    continuing self portrait, just started outside

--- topics/Assignments/Art Jam/js/script.js
+++ topics/Assignments/Art Jam/js/script.js
@@ -7,6 +7,7 @@
 
 "use strict";
 
+
 /**
  * creates a canvas
 */
@@ -24,11 +25,48 @@ function draw() {
 
     background(240, 235, 235);//wall colour
     drawDesk(); // draws the desk
+    drawOutside();//draws the outside world changing
+    drawWindow(); // draws the window frame
 
 }
 function drawDesk() {
     push();
     fill("#8e857c");
     rect(30, 400, 580, 200);
+    pop();
+}
+function drawOutside() {
+
+}
+function drawWindow() {
+    //left vertical
+    push();
+    fill("white");
+    noStroke();
+    rect(50, 50, 10, 200)
+    pop();
+    //right vertical
+    push();
+    fill("white");
+    noStroke();
+    rect(200, 50, 10, 200)
+    pop();
+    //top horizontal
+    push();
+    fill("white");
+    noStroke();
+    rect(50, 39, 160, 10)
+    pop();
+    //bottom top
+    push();
+    fill("white");
+    noStroke();
+    rect(30, 250, 200, 10)
+    pop();
+    //bottom bottom
+    push();
+    fill("white");
+    noStroke();
+    rect(50, 261, 160, 10)
     pop();
 }
