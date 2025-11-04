4912d88ba51bba48536ab9241aed25002c2ed773
crooksnwhiskey <alex.chardon36@gmail.com>
2025-11-03 19:42:48 -0500

    added instructions to script.js

--- topics/assignments/mod-jam/frogfrogfrog/js/script.js
+++ topics/assignments/mod-jam/frogfrogfrog/js/script.js
@@ -5,9 +5,17 @@
  * A game of catching flies with your Phraug-tongue
  * 
  * Instructions:
- * - Move the frog with your mouse
- * - Click to launch the tongue
- * - Catch flies
+ * The goal of Phraug is to stay alive as long as possible while getting the highest score possible. Click your mouse button to extend tongue and try to catch flies.
+
+There are 3 ways to die in this game:
+
+First, if you let your eyes (controlled with a & d) touch the ground, you die. 
+
+Second, if the frog touches the edges of the screen, you die. 
+To move the frog, place your cursor on either the left of the screen to go right, or place your cursor on the right side of the screen to go left
+
+Third, if your score reaches 0, you die.
+Your score is always going down, BUT: if you catch a fly, your score goes up, if you let a fly reach the end, your score goes down.
  * 
  * Made with p5
  * https://p5js.org/
